bcb3a10337dd6388e24bf2a877d4ae2bb96d222a
eljorgecruz <jcruz42@uco.mx>
2025-05-11 20:00:53 -0600

    Feat: configuration of DTOs class

---
+++ LoginJWT/Models/DTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace LoginJWT.Models.DTOs
+{
+    public class UserDTO
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}

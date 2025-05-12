d4c9ce81d997765cd2f3247f748100cbbb074d0a
eljorgecruz <jcruz42@uco.mx>
2025-05-11 19:23:55 -0600

    Feat: utilities functions to manage JWT

---
+++ LoginJWT/Custom/Utilities.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using LoginJWT.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace LoginJWT.Custom
+{
+    public class Utilities
+    {
+        private readonly IConfiguration _configuration;
+        public Utilities(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string encryptSHA256(string text)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // computing hash
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+                // Convert bytes to string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+
+            }
+        }
+
+        public string genarateJWT(User model)
+        {
+            // User token data
+            var userClaims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, model.IdUser.ToString()),
+                new Claim(ClaimTypes.Email, model.Email!)
+            };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:key"]!));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            // create token detail
+            var jwtConfig = new JwtSecurityToken(
+                claims: userClaims,
+                expires: DateTime.UtcNow.AddMinutes(10),
+                signingCredentials: credentials
+            );
+        }
+
+    }
+}
